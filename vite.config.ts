a3428014f22cd52cb076e46b0630e8ebcc7aa92b
Oleksandr Cheban <cebanoleksandr@gmail.com>
2025-12-19 12:11:11 +0200

    Fix errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/usense-react-wheather/',
+  base: '/usense-react-weather/',
   plugins: [
     react(),
     tailwindcss()
